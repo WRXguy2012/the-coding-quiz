d9b44b161c558a048dea6bb3b647b8fd80742793
wrxguy2012 <lovin.jacob@gmail.com>
2023-01-19 14:41:29 -0500

    JS work

--- the-coding-quiz2/assets/script.js
+++ the-coding-quiz2/assets/script.js
@@ -68,19 +68,21 @@ function showResults(){
     });
   
     // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    resultsContainer.innerHTML = (`<div> 
+                            <p> You got ${numCorrect} out of ${myQuestions.length} </p>
+                            <a href="../index.html">Try Again?</a>`);
 }
 
 function showSlide(n) {
     slides[currentSlide].classList.remove('active-slide');
     slides[n].classList.add('active-slide');
     currentSlide = n;
-    if(currentSlide === 0){
-      previousButton.style.display = 'none';
-    }
-    else{
-      previousButton.style.display = 'inline-block';
-    }
+    // if(currentSlide === 0){
+    //   previousButton.style.display = 'none';
+    // }
+    // else{
+    //   previousButton.style.display = 'inline-block';
+    // }
     if(currentSlide === slides.length-1){
       nextButton.style.display = 'none';
       submitButton.style.display = 'inline-block';
@@ -89,7 +91,7 @@ function showSlide(n) {
       nextButton.style.display = 'inline-block';
       submitButton.style.display = 'none';
     }
-  }
+}
 
   function showNextSlide() {
     showSlide(currentSlide + 1);
@@ -99,11 +101,11 @@ function showSlide(n) {
     showSlide(currentSlide - 1);
   }
 
-
 // Variables
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
+const time = document.getElementById('time');
 const myQuestions = [
     {
       question: "",
@@ -138,16 +140,16 @@ const myQuestions = [
 buildQuiz();
 
 // Pagination
-const startButton = document.getElementById("startQuiz")
 const previousButton = document.getElementById("previous");
 const nextButton = document.getElementById("next");
 const slides = document.querySelectorAll(".slide");
 let currentSlide = 0;
 
-// showSlide(currentSlide);
+showSlide(currentSlide);
 
 // Event listeners
-startButton.addEventListener('click', buildQuiz, showSlide(currentSlide));
 submitButton.addEventListener('click', showResults);
-previousButton.addEventListener("click", showPreviousSlide);
+// previousButton.addEventListener("click", showPreviousSlide);
 nextButton.addEventListener("click", showNextSlide);
+const timeoutId = setTimeout(showResults, 30000);
+time.innerHTML = (`${timeoutId}`);
